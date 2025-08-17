935054681b1bbb0987ead9b60da2dd5ab8b6f849
Sandaruwani Lasanga <karaliyawattalasanga@gmail.com>
2025-08-18 01:13:48 +0530

    Update DatabaseConnection utility for DB operations

--- src/main/java/com/pahanedu/bookshop/util/DatabaseConnection.java
+++ src/main/java/com/pahanedu/bookshop/util/DatabaseConnection.java
@@ -5,11 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static DatabaseConnection instance;
     private static final String URL = "jdbc:mysql://localhost:3306/pahan_edu_bookshop";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     
-    static {
+    // Private constructor to prevent instantiation
+    private DatabaseConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -17,11 +19,19 @@ public class DatabaseConnection {
         }
     }
     
-    public static Connection getConnection() throws SQLException {
+    // Public static method to get the singleton instance
+    public static synchronized DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+    
+    public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
     
-    public static void closeConnection(Connection connection) {
+    public void closeConnection(Connection connection) {
         if (connection != null) {
             try {
                 connection.close();
